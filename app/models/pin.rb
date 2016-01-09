311dd5ea4f0fd33c2a445b22b8ed1adce6bb2318
Ziv Zamechek <ziv921@yahoo.com>
2016-01-09 15:17:02 -0500

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,6 @@ class Pin < ActiveRecord::Base
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
      validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 
- validates :image, presence: true
+  validates :image, presence: true
   validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105024125) do
+ActiveRecord::Schema.define(version: 20160109200350) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160105024125) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
